5421704f93821ad939b2db5fec6f0a2df7e6fd10
Antony <meiyixuan2000@126.com>
2022-07-22 16:40:01 +0800

    finished initial test

--- src/scheduler.py
+++ src/scheduler.py
@@ -6,15 +6,15 @@ import os
 import time
 
 import zmq
-import config
-from utils import serialize, deserialize
+import src.config as config
+from src.utils import serialize, deserialize
 
 
 def master():
     # initialize zmq socket
     zmq_context = zmq.Context()
     master_listen_socket = zmq_context.socket(zmq.PULL)
-    master_address = f"tcp://{config.master_ip}:{config.master_port_inside}"
+    master_address = f"tcp://*:{config.master_port_inside}"
     master_listen_socket.bind(master_address)
     print(f"[{os.getpid()}] Master listens at {config.master_port_inside}.")
 
@@ -22,7 +22,7 @@ def master():
     while True:
         raw_msg = master_listen_socket.recv()
         msg = deserialize(raw_msg)
-        print(raw_msg)
+        print(msg)
 
 
 def worker():
@@ -30,7 +30,7 @@ def worker():
     zmq_context = zmq.Context()
     master_listen_socket = zmq_context.socket(zmq.PUSH)
     master_address = f"tcp://{config.master_ip}:{config.master_port_outside}"
-    master_listen_socket.bind(master_address)
+    master_listen_socket.connect(master_address)
     print(f"[{os.getpid()}] Contact master at {master_address}.")
 
     # main loop
@@ -39,4 +39,5 @@ def worker():
         msg = f"Client Message {counter}"
         master_listen_socket.send(serialize(msg))
         print(f"Client sends: {msg}")
+        counter += 1
         time.sleep(1)
